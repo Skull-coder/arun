2cea1f805d3f41e90291f5b51fd209441f746964
Skull-coder <skullthecoder@gmail.com>
2026-07-04 16:48:44 +0530

    deleted unwated test files

--- test-pg.js
+++
@@ -1,5 +0,0 @@
-require('dotenv').config();
-const { parse } = require('pg-connection-string');
-const config = parse(process.env.DATABASE_URL);
-console.log('Parsed config:', config);
-console.log('Type of password:', typeof config.password);
