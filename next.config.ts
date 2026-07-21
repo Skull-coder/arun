88a7422481a1bd4670e32c5b744fc1b04c855c76
Skull-coder <skullthecoder@gmail.com>
2026-07-21 14:29:32 +0530

    auto deploy on vps

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  allowedDevOrigins: ['angelic-covenant-shriek.ngrok-free.dev'],
+  allowedDevOrigins: ['angelic-covenant-shriek.ngrok-free.dev', 'arun.skullcoder.dev'],
 };
 
 export default nextConfig;
